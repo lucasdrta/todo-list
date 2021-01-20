aca9f3b8a7a71fee1b9a6730488b916d7c0d5f40
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-20 20:27:55 -0300

    Create route to list all uncompleted todos

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -6,6 +6,7 @@ const todoController = new TodosController();
 
 routes.get('/', todoController.list);
 routes.get('/done', todoController.listCompletedTodos);
+routes.get('/undone', todoController.listUncompletedTodos);
 routes.post('/', todoController.create);
 routes.put('/:id', todoController.update);
 
