43b44ddb725a817239b9a69c6581b6107c27a425
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-20 15:06:37 -0300

    Create authenticate method

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import AuthService from '../services/auth';
 
 const prisma = new PrismaClient();
 
@@ -23,5 +24,30 @@ export class UserController {
         message: error.message,
       });
     }
+  }
+
+  public async authenticate(
+    req: Request,
+    res: Response
+  ): Promise<void | undefined> {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      res.status(401).send({
+        code: 401,
+        message: 'User not found',
+      });
+    }
+
+    if (password !== user.password) {
+      res.status(401).send({
+        code: 401,
+        message: 'Password does not match!',
+      });
+    }
+
+    const token = AuthService.generateToken(user);
+    res.status(200).send({ token });
   }
 }
