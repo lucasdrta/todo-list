8f424eb4867f794799d3fbd8e46e6b3d4428a1ae
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-20 20:08:41 -0300

    Create update route to complete a todo

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -6,5 +6,6 @@ const todoController = new TodosController();
 
 routes.get('/', todoController.list);
 routes.post('/', todoController.create);
+routes.put('/:id', todoController.update);
 
 export default routes;
