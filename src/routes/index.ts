61dfd6d944ddbbf0e8a7cf4c6663147000b0ab54
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-20 15:24:46 -0300

    Use auth middleware in todo route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import { authMiddleware } from '../middlewares/auth';
+
 const routes = Router();
 
 import user from './user';
 import todos from './todo';
 
 routes.use('/users', user);
-routes.use('/todos', todos);
+routes.use('/todos', authMiddleware, todos);
 
 export default routes;
