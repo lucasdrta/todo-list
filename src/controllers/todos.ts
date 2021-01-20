f1815826cb06b08554e07e5765a72daccd63f4fc
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-20 17:56:05 -0300

    Add method to list all todos

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -4,6 +4,25 @@ import { Request, Response } from 'express';
 const prisma = new PrismaClient();
 
 export class TodosController {
+  public async list(req: Request, res: Response): Promise<void> {
+    try {
+      const decoded = Object.values(req.decoded);
+
+      const todo = await prisma.todo.findMany({
+        where: {
+          userId: decoded[0],
+        },
+      });
+
+      res.status(200).send(todo);
+    } catch (error) {
+      res.status(400).send({
+        code: 400,
+        message: error.message,
+      });
+    }
+  }
+
   public async create(req: Request, res: Response): Promise<void> {
     try {
       const { name } = req.body;
