43b102a2a72bf3a5c5d769a4355a802e75ce8d42
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-01-20 17:56:13 -0300

    Add route to list all todos

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -4,6 +4,7 @@ import { TodosController } from '../controllers/todos';
 const routes = Router();
 const todoController = new TodosController();
 
+routes.get('/', todoController.list);
 routes.post('/', todoController.create);
 
 export default routes;
